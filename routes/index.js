49a71fdb21df0c5ba64b4fac2c3944880f79c8e4
Roland Mechler <rmechler@gmail.com>
2013-10-16 21:48:18 -0700

    made adding pages dynamically work

--- routes/index.js
+++ routes/index.js
@@ -29,17 +29,58 @@ var updateLinks = function(req, res, item, callback) {
 	}
 };
 
+var addPage = function(req, res, item, callback) {
+	pageStore.update({name: req.body.name}, {$set: {name: req.body.name, title: req.body.title}}, { upsert: true }, function(err, count) {
+	  callback(err);
+	});
+};
+
+var updatePages = function(req, res, item, callback) {
+	if (req.body && req.body.name && req.body.title) {
+		addPage(req, res, item, function(err) {
+			if (err) {
+				console.log('Unable to add page!', err);
+				callback(err);
+			} else {
+				if (!item.pages) {
+					item.pages = [];
+				}
+				item.pages.push({name: req.body.name, title: req.body.title})
+				pageStore.update({name: req.params.pageName}, item, { upsert: true }, function(err, count) {
+				  callback(err);
+				});
+			}
+		});
+	} else {	
+		callback(null);
+	}
+};
+
 var renderPage = function(req, res) {
+	console.log(req.params.pageName)
 	pageStore.findOne({name:req.params.pageName}, function(err, item) {
+		console.log(err)
 	  if (err || item == null) {
 	    res.send('Page not found!', 404);
 	  } else {
-	  	updateLinks(req, res, item, function(err) {
+	  	updatePages(req, res, item, function(err) {
 	  		if (err) {
-			    console.log('Unable to update links!', err);
+			    console.log('Unable to update pages!', err);
 			  }
-	  		res.render('page', item);
-	  	});
+		  	updateLinks(req, res, item, function(err) {
+		  		if (err) {
+				    console.log('Unable to update links!', err);
+				  }
+				  // TODO: the below wouldn't be needed if can check if variable exiss in jade template... investigate
+				  if (!item.links) {
+				  	item.links = [];
+				  }
+				  if (!item.pages) {
+				  	item.pages = [];
+				  }
+		  		res.render('page', item);
+		  	});
+		  });
 	  }
 
 	});
